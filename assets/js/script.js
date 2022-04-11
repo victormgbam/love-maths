7484765c270b0445e9ae049c064a52243f4c1bd8
victormgbam <victormgbam@hotmail.com>
2022-04-11 09:14:07 +0000

    Dispaly calculate correct answer to the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -40,8 +40,22 @@ function checkAnswer() {
 
 }
 
-function calculateCorrectAnswer() {
+/**
+ * Get the operands (the numbers) and the operator (plus, minus, etc)
+ * directly from the DOM, and return the correct answer
+ */
 
+function calculateCorrectAnswer() {
+   let operand1 = parseInt(document.getElementById("operand1").innerText);
+   let operand2 = parseInt(document.getElementById("operand2").innerText);
+   let operator = document.getElementById("operator").innerText;
+
+   if (operator === "+") {
+      return[operand1 + operand2, "addition"]
+   }else {
+     alert(`unimplemented operator ${operator}`);
+     throw `unimplemented operator ${operator}.Aborting!`;
+   }
 }
 
 function incrementScore() {
