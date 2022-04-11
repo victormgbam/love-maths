7acdb6acfb2cfb0cffcc3f8cdc84de908af2d670
victormgbam <victormgbam@hotmail.com>
2022-04-11 12:04:49 +0000

    Add final tidy up for user

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,9 +14,16 @@ document.addEventListener("DOMContentLoaded", function() {
         }
     })
   }
+    
+  document.getElementById("answer-box").addEventListener("keydown", function(event) {
+    if (event.key === "Enter") {
+      checkAnswer();
+    }
+})
+    
+    runGame("addition");
 })
 
-runGame("addition");
 
 /**
  * The main game "loop", called when the script is first loaded
@@ -24,6 +31,10 @@ runGame("addition");
  */
 
 function runGame(gameType) {
+
+  document.getElementById("answer-box").value = "";
+  document.getElementById("answer-box").focus();
+
     // Creating Two Random Numbers between 1 and 25 
    let num1 = Math.floor(Math.random() * 25) + 1;
    let num2 = Math.floor(Math.random() * 25) + 1;
